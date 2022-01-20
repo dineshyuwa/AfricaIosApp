78e120b7cb7ce5e60d36f6211fe7d9879b118d89
Yuwarajalingam Dinesh <yuwarajalingamd@gmail.com>
2022-01-20 10:55:20 +0530

    Added tabView

---
+++ Africa/App/GalleryView.swift
@@ -0,0 +1,20 @@
+//
+//  GalleryView.swift
+//  Africa
+//
+//  Created by Yuwarajalingam Dinesh on 2022-01-20.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text("GalleryView")
+    }
+}
+
+struct GalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryView()
+    }
+}
---
+++ Africa/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Yuwarajalingam Dinesh on 2022-01-20.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("MapView")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
